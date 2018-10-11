eabff641365eed04737453fad09109e3fd243dcd
lukini <lucas_masciotra@hotmail.com>
2018-10-11 12:23:27 -0300

    here we go

---
+++ config/database.js
@@ -0,0 +1,6 @@
+var MongoClient = require('mongodb').MongoClient;
+module.exports = {
+    'secret':'meansecure',
+    'database': 'mongodb+srv://lukini:959933@lukinismongo-9hbqe.mongodb.net/test?retryWrites=true'
+  };
+
